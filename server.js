b12985668e068b48774074ea46ecbe4a3eac0a00
thuerand <thuerand@students.zhaw.ch>
2025-05-15 20:23:51 +0200

    changing imageName

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Connecting every thing together!');
+  res.send('Hello FS2025 DevOps Course! Newly checking Azure with Docker!');
 });
 
 app.listen(PORT, HOST);
